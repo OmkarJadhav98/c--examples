bb70eed53d8163134a65e1f00cc52b305f5ba0e3
unknown <objadhav28@gmail.com>
2024-04-06 20:58:15 +0530

    Division Program

---
+++ Division.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    int No1 = 10;
+    int No2 = 20;
+    int INTEGER = No1 / No2;
+
+    long iLong1 = 32151;
+    long iLong2 = 651651;
+    long LONG = iLong1 / iLong2;
+
+    float No3 = 22.22f;
+    float No4 = 10.10f;
+    float FLOAT = No3 / No4;
+
+    double iNo1 = 10.11111;
+    double iNo2 = 20.11111;
+    double DOUBLE = iNo1 / iNo2;
+
+    char iNo3 = '8';
+    char iNo4 = '2';
+    int CHAR = (iNo3 - '0') / (iNo4 - '0');
+
+    cout << "Division of integer is : " << INTEGER <<'\n'; 
+    cout << "Division of long is : " << LONG <<'\n';
+    cout << "Division of float is : " << FLOAT <<'\n';
+    cout << "Division of double is : " << DOUBLE <<'\n'; 
+    cout << "Division of character is : " << CHAR <<'\n';  
+
+    return 0;
+}
