22f6a7bb9f658517e3aaa8f78dedae98d31dceb4
unknown <objadhav28@gmail.com>
2024-04-30 21:24:39 +0530

    Using While loop

---
+++ PrimeNumber/1_1.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+bool isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+    if (num == 2)
+    {
+        return true;
+    }
+    
+    int i = 2;
+    while (i <= sqrt(num))
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+int main()
+{
+    int number = 0;
+    
+    cout << "Enter a number to check if it's prime: \n";
+    cin >> number;
+
+    if (isPrime(number))
+    {
+        cout << number << " is a prime number.\n";
+    }
+    else
+    {
+        cout << number << " is not a prime number.\n";
+    }
+    return 0;
+}
+
+
